08431e1de0407aaab2c3dd224664a105b86e851e
anjali19999 <patukuri.anjali@kasmo.co>
2023-08-16 12:30:33 +0530

    adding file

---
+++ Devops.py
@@ -0,0 +1,20 @@
+
+import pyspark
+from pyspark.sql import SparkSession
+import pandas
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+data = [("James","","Smith","36636","M",60000),
+        ("Michael","Rose","","40288","M",70000),
+        ("Robert","","Williams","42114","",400000),
+        ("Maria","Anne","Jones","39192","F",500000),
+        ("Jen","Mary","Brown","","F",0)]
+
+columns = ["first_name","middle_name","last_name","dob","gender","salary"]
+pysparkDF = spark.createDataFrame(data = data, schema = columns)
+pysparkDF.printSchema()
+pysparkDF.show(truncate=False)
+
+pandasDF = pysparkDF.toPandas()
+print(pandasDF)
