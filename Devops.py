7ecbc7b7527615b5295e07073632b3e812bd1e4f
anjali19999 <134581255+anjali19999@users.noreply.github.com>
2023-08-21 12:49:05 +0530

    Update Devops.py

--- Devops.py
+++ Devops.py
@@ -9,7 +9,8 @@ data = [("James","","Smith","36636","M",60000),
         ("Michael","Rose","","40288","M",70000),
         ("Robert","","Williams","42114","",400000),
         ("Maria","Anne","Jones","39192","F",500000),
-        ("Jen","Mary","Brown","","F",0)]
+        ("Jen","Mary","Brown","","F",0),
+       ("nick","rose","","F",123)]
 
 columns = ["first_name","middle_name","last_name","dob","gender","salary"]
 pysparkDF = spark.createDataFrame(data = data, schema = columns)
